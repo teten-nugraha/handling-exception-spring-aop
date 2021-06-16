a5a65effae08a3b5594c905aa01273da8ca6410c
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-06-16 09:15:15 +0700

    merge

---
+++ src/main/java/id/ten/handlingexceptionaop/HandlingExceptionAopApplication.java
@@ -0,0 +1,13 @@
+package id.ten.handlingexceptionaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingExceptionAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlingExceptionAopApplication.class, args);
+	}
+
+}
